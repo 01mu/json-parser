fd5f3f032cf49999b52061e63fb9754453698f89
01mu <40478591+01mu@users.noreply.github.com>
2018-06-22 00:28:47 -0400

    Rename main.cpp to src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "stdafx.h"
+
+#include <iostream>
+#include "parser.h"
+
+using namespace std;
+
+int main()
+{
+    json thing = json("json.txt");
+
+    cout << thing.get("[glossary][title]");
+
+    return 0;
+}
